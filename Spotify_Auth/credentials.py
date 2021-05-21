c32a65ccd9ab5357f282b03d00cc34ee0afe8175
Rohan Yadav <drohanyadav@gmail.com>
2021-05-21 17:31:22 +0530

    fix

--- Spotify_Auth/credentials.py
+++ Spotify_Auth/credentials.py
@@ -3,7 +3,7 @@ CLIENT_SECRET = '25f201ab166d46169165bbd4aad652ee'
 
 SCOPE = """
         user-read-playback-state user-read-currently-playing user-modify-playback-state
-        playlist-read-private playlist-read-collaborativ
+        playlist-read-private playlist-read-collaborative
         """
         
 REDIRECT_URI = 'http://localhost:8000/login/redirect'
